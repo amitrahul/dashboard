fe897ba037eadd83d626d9bacc7b6163e323465d
Rahul Raj <raj.rahul686@gmail.com>
2025-02-02 17:49:27 +0530

    Created Dark Mode context for global theme management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import Header from "./components/Header";
+import { ThemeProvider } from "./context/theme";
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
   return (
-    <>
-      <h1 className="text-5xl text-red-700">dashboard</h1>
-    </>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <Header />
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import DarkModeToggle from "./DarkModeToggle";
+
 function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center ">
       <h1 className="text-xl font-bold">Dashboard</h1>
+      <DarkModeToggle />
     </header>
   );
 }
